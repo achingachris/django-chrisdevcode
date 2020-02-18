44b9cf1053236adf644afcd5336375220a0c3999
ChrisAchinga <achinga.chris@gmail.com>
2020-02-18 17:15:37 +0300

    number types

---
+++ Data-Types/boolean/TrueFalse.java
@@ -0,0 +1,10 @@
+// Java program to demonstrate boolean data type 
+  
+class TrueFalse { 
+    public static void main(String args[]) 
+    { 
+        boolean b = true; 
+        if (b == true) 
+            System.out.println("Hi Geek"); 
+    } 
+} 
