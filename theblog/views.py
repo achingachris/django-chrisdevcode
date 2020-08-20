d4e4fbaf35a2a4f6b5cf24f51df69f1614424d5e
ChrisAchinga <achinga.chris@gmail.com>
2020-08-20 22:31:55 +0300

    slugify category link

--- theblog/views.py
+++ theblog/views.py
@@ -43,5 +43,5 @@ class DeletePost(DeleteView):
 
 # functional views
 def CategoryView(request, cats):
-    category_post = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats':cats.title(), 'category_post':category_post})
+    category_post = Post.objects.filter(category=cats.replace('-',' '))
+    return render(request, 'categories.html', {'cats':cats.title().replace('-',' '), 'category_post':category_post})
