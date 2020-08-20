17196f86a9a9dc259608b3975ff9d9f7ccd49189
ChrisAchinga <achinga.chris@gmail.com>
2020-08-20 15:29:49 +0300

    urls

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 # from . import views
-from .views import homeView, detailView
+from .views import homeView, detailView, AddPost, UpdatePost
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', homeView.as_view(), name='home'),
-    path('article/<int:pk>', detailView.as_view(), name='article-detail')
+    path('article/<int:pk>', detailView.as_view(), name='article-detail'),
+    path('add/', AddPost.as_view(), name='add'),
+    path('article/edit/<int:pk>', UpdatePost.as_view(), name='update'),
 ]
